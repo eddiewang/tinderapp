d7a68e0c17792964144b2599bbc403f85ef5025e
Eddie Wang <bonjour@eddiewang.me>
2016-06-20 14:13:40 -0400

    tinder endpoints

--- server/testApi.js
+++ server/testApi.js
@@ -47,4 +47,56 @@ router.get('/tinder/recommendations', tinderauth, (req, res) => {
   });
 });
 
+/*
+  Posts User Like to Tinder
+*/
+router.post('/tinder/like/:id', tinderauth, (req, res) => {
+  client.like(req.params.id, (err, liked) => {
+    if (err) res.status(404).json(err);
+    res.json(liked);
+  });
+});
+
+/*
+  Posts User Pass to Tinder
+*/
+
+router.post('/tinder/pass/:id', tinderauth, (req, res) => {
+  client.pass(req.params.id, (err, passed) => {
+    if (err) res.status(404).json(err);
+    res.json(passed);
+  })
+});
+
+/*
+****LIKE RESULTS****
+{
+  "match": {
+    "_id": "574de9db2ad776210ccdcb635767bd15c965c4103cc1026d",
+    "closed": false,
+    "common_friend_count": 0,
+    "common_like_count": 0,
+    "created_date": "2016-06-20T17:29:09.093Z",
+    "dead": false,
+    "last_activity_date": "2016-06-20T17:29:09.093Z",
+    "message_count": 0,
+    "messages": [],
+    "participants": [
+      "574de9db2ad776210ccdcb63",
+      "5767bd15c965c4103cc1026d"
+    ],
+    "pending": false,
+    "is_super_like": false,
+    "following": true,
+    "following_moments": true
+  },
+  "likes_remaining": 100
+}
+
+{
+  "match": false,
+  "likes_remaining": 100
+}
+*/
+
 module.exports = router;
